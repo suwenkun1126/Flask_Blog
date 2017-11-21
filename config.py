a022b6fb2d104ad78cb187d10b9eb2b0122834b8
unknown <suwenkun1126@126.com>
2017-11-21 17:40:51 +0800

    first mondify

---
+++ config.py
@@ -0,0 +1,75 @@
+import os
+basedir=os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY=os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN=True
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'
+    FLASKY_MAIL_SENDER='378733604@qq.com'
+    # FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN')
+    FLASKY_ADMIN='378733604@qq.com'
+    FLASKY_COMMENTS_PER_PAGE=10
+    FLASKY_POSTS_PER_PAGE=10
+    SQLCHEMY_RECORD_QUERIES=True
+    FLASKY_SLOW_DB_QUERY_TIME=0.5
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    MAIL_SERVER='smtp.qq.com'
+    MAIL_PORT=465
+    MAIL_USE_TLS=False
+    MAIL_USE_SSL=True
+    # MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    MAIL_USERNAME='37xxxxxxx@qq.com'
+    # MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    MAIL_PASSWORD='kvxxxxxxxxxxxx'
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING=True
+    WTF_CSRF_ENABLED=False
+    SQLALCHEMY_DATABASE_URI=os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    @classmethod
+    def init_app(cls,app):
+        Config.init_app(app)
+
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials=None
+        secure=None
+        if getattr(cls,'MAIL_USERNAME',None) is not None:
+            credentials=(cls.MAIL_USERNAME,cls.MAIL_PASSWORD)
+            if getattr(cls,'MAIL_USE_TLS',None):
+                secure=()
+        mail_handler=SMTPHandler(
+            mailhost=(cls.MAIL_SERVER,cls.MAIL_PORT),
+            fromaddr=cls.FLASKY_MAIL_SENDER,
+            toaddrs=[cls.FLASKY_ADMIN],
+            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX+'Application Error',
+            credentials=credentials,
+            secure=secure
+             )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+
+
+
+config={
+    'development':DevelopmentConfig,
+    'testing':TestingConfig,
+    'production':ProductionConfig,
+
+    'default':DevelopmentConfig}
---
+++ tests/test_client.py
@@ -0,0 +1,49 @@
+import unittest
+from app import create_app,db
+from app.models import User,Role
+from flask import url_for
+import re
+
+class FlaskClientTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app=create_app('testing')
+        self.app_context=self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        self.client=self.app.test_client(use_cookies=True)
+
+    def tearDown(self):
+        db.session.remove()
+        self.app_context.pop()
+
+    def test_home_page(self):
+        response=self.client.get(url_for('main.index'))
+        self.assertTrue('Stranger' in response.get_data(as_text=True))
+
+    def test_register_and_login(self):
+        response=self.client.post(url_for('auth.register'),data={
+            'email':'john@example.com',
+            'username':'john',
+            'password':'cat',
+            'password2':'cat',
+        })
+        self.assertTrue(response.status_code==302)
+
+        response=self.client.post(url_for('auth.login'),data={
+            'email':'john@example.com',
+            'password':'cat',
+        },follow_redirect=True)
+        data=response.get_data(as_text=True)
+        self.assertTrue(re.search('Hello,\s+john!',response.get_data(as_text=True)))
+        self.assertTrue('You have not confirmed your account yet' in response.get_data(as_text=True))
+
+        user=User.query.filter_by(eamil='john@example.com').first()
+        token=user.generate_confirmation_token()
+        response=self.client.get(url_for('auth.confirm',token=token),follow_redirect=True)
+        data=response.get_data(as_text=True)
+        self.assertTrue('You have confirmed your account' in response.get_data(as_text=True))
+
+        response=self.client.get(url_for('auth.logout'),follow_redirect=True)
+        data=response.get_data(as_text=True)
+        self.assertTrue('You have been logged out' in response.get_data(as_text=True))
