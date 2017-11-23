e27696fa3d3387e120c6f4b8a50bb1e5aa96ef05
suwenkun1126 <suwenkun1126@126.com>
2017-11-23 21:10:43 +0800

    second modify

--- config.py
+++ config.py
@@ -28,18 +28,21 @@ class DevelopmentConfig(Config):
     # MAIL_USERNAME='37xxxxxxx@qq.com'
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
     # MAIL_PASSWORD='kvxxxxxxxxxxxx'
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    # SQLALCHEMY_DATABASE_URI=os.environ.get('DEV_DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DEV_DATABASE_URL')
 
 class TestingConfig(Config):
     TESTING=True
     WTF_CSRF_ENABLED=False
-    SQLALCHEMY_DATABASE_URI=os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    # SQLALCHEMY_DATABASE_URI=os.environ.get('TEST_DATABASE_URL') or \
+    #     'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('TEST_DATABASE_URL')
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    # SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or \
+    #                           'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
     @classmethod
     def init_app(cls,app):
         Config.init_app(app)
